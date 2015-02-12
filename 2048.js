36ff397b0d0966e7ca3e2e4641662844516e4d88
worace <horace.d.williams@gmail.com>
2015-02-11 22:49:03 -0700

    restructure to provide more types

--- 2048.js
+++ 2048.js
@@ -1,6 +1,7 @@
 keyCode = function(dir) {
   return {37: "left", 38: "up", 39: "right", 40: "down", left: 37, up: 38, right: 39, down:40}[dir];
 }
+
 var Utils = {
   rand: function(length) {
     return Math.floor((Math.random() * (length - 1)));
@@ -10,57 +11,93 @@ var Utils = {
   }
 }
 
+function Square(val) {
+  this.value = val;
+}
+
+Square.prototype.htmlContent = function() {
+  if (this.empty()) {
+    return ""
+  } else {
+    return this.value.toString();
+  }
+}
+
+Square.prototype.empty = function() {
+  return this.value === 0;
+}
+
+Square.prototype.render = function($element) {
+  $element.append(this.domElement());
+}
+
+Square.prototype.domElement = function() {
+  var square = document.createElement("div");
+  var $sq = $(square);
+  $sq.addClass("square");
+  $sq.html(this.htmlContent())
+  return $sq;
+}
+
+function Grid(width, height) {
+  this.width = width;
+  this.height = height;
+  this.numSquares = width * height;
+  this.startingVals = [2,4];
+  this.squares = [];
+  this.populateGrid()
+}
+
+Grid.prototype.populateGrid = function() {
+  var preFilledIndices = this.preFilledIndices(this.width * this.height);
+  for(var i = 0; i < this.width * this.height; i++) {
+    if (Utils.contains(preFilledIndices, i)) {
+      this.squares.push(new Square(this.startingVal()));
+    } else {
+      this.squares.push(new Square(0));
+    }
+  }
+}
+
+Grid.prototype.startingVal = function() {
+  return this.startingVals[Utils.rand(this.startingVals.length - 1)];
+}
+
+Grid.prototype.preFilledIndices = function(length) {
+  var i1 = Utils.rand(length);
+  var i2 = i1;
+
+  while(i2 === i1) {
+    i2 = Utils.rand(length);
+  }
+  return [i1,i2];
+}
+
+Grid.prototype.render = function($element) {
+  for (var i in this.squares) {
+    this.squares[i].render($element);
+  }
+}
+
 function TFE() {
   this.width = 4;
   this.height = 4;
-  this.startingVals = [2,4];
-
-  this.init = function(rootSelector) {
-    this.$root = $(rootSelector);
-    this.populateGrid();
-    this.attachKeyListeners();
-  }
-
-  this.keyPress = function(dir) {
-    if (dir === undefined) return;
-  }
-
-  this.attachKeyListeners = function() {
-    $("body").keydown(function(event) {
-      this.keyPress(keyCode(event.which));
-    }.bind(this));
-  }
-
-  this.populateGrid = function() {
-    var filledIndices = this.filledIndices(this.width * this.height);
-    for(var i = 0; i < this.width * this.height; i++) {
-      if (Utils.contains(filledIndices, i)) {
-        this.$root.append(this.newSquare(this.startingVal().toString()));
-      } else {
-        this.$root.append(this.newSquare(""));
-      }
-    }
-  }
-
-  this.startingVal = function() {
-    return this.startingVals[Utils.rand(this.startingVals.length - 1)];
-  }
-
-  this.filledIndices = function(length) {
-    var i1 = Utils.rand(length);
-    var i2 = i1;
-
-    while(i2 === i1) {
-      i2 = Utils.rand(length);
-    }
-    return [i1,i2];
-  }
-
-  this.newSquare = function(content) {
-    var square = document.createElement("div");
-    var $sq = $(square);
-    $sq.addClass("square");
-    $sq.html(content)
-    return $sq;
-  }
+  this.grid = new Grid(this.width, this.height);
 }
+
+TFE.prototype.init = function(rootSelector) {
+  this.$root = $(rootSelector);
+  this.grid.render(this.$root);
+  this.attachKeyListeners();
+}
+
+TFE.prototype.keyPress = function(dir) {
+  if (dir === undefined) return;
+}
+
+TFE.prototype.attachKeyListeners = function() {
+  $("body").keydown(function(event) {
+    this.keyPress(keyCode(event.which));
+  }.bind(this));
+}
+
--- 2048_test.js
+++ 2048_test.js
@@ -20,7 +20,60 @@ QUnit.test("it finds arrow key codes", function( assert ) {
   assert.equal(40, keyCode("down"));
 });
 
-QUnit.test("it fills another square on arrow key press", function( assert ) {
+// Grid
+QUnit.test("it inits with width and height", function( assert ) {
+  assert.equal(4, new Grid(4).width);
+  assert.equal(5, new Grid(4, 5).height);
+});
+
+QUnit.test("it populates with 2 pre-filled squares", function( assert ) {
+  var g = new Grid(4,4)
+  var zeros = [];
+  var others = [];
+  for (i in g.squares) {
+    if (g.squares[i].empty()) {
+      zeros.push(g.squares[i]);
+    } else {
+      others.push(g.squares[i]);
+    }
+  }
+
+  assert.equal(2, others.length);
+  assert.equal(14, zeros.length);
+});
+
+
+// Square
+
+QUnit.test("it inits with val", function( assert ) {
+  assert.equal(15, new Square(15).value);
+});
+
+QUnit.test("it finds htmlContent", function( assert ) {
+  assert.equal("15", new Square(15).htmlContent());
+  assert.equal("", new Square(0).htmlContent());
+});
+
+QUnit.test("it is empty if val is 0", function( assert ) {
+  assert.ok(!(new Square(15).empty()));
+  assert.ok(new Square(0).empty());
+});
+
+QUnit.test("it generates a dom element", function( assert ) {
+  var sq = new Square(5);
+  var $elem = sq.domElement();
+  assert.equal("5", $elem.html());
+  assert.ok($elem.hasClass("square"));
+});
+
+QUnit.test("it renders itself onto a dom element", function( assert ) {
+  var $elem = $(document.createElement("div"));
+  assert.equal(0, $elem.children().length);
+  new Square(5).render($elem);
+  assert.equal(1, $elem.children(".square").length);
+});
+
+QUnit.skip("it fills another square on arrow key press", function( assert ) {
   new TFE().init("#grid-root");
   assert.equal(2, filledSquares().length);
   triggerKeyPress("left");
